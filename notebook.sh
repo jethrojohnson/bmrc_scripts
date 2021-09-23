52909c8dd066cabe186660ed7393103bede8afd3
Jethro Johnson <ovz380@rescomp1.hpc.in.bmrc.ox.ac.uk>
2021-09-23 20:00:23 +0100

    Added qsub for jupyter notebook sessions

---
+++ notebook.sh
@@ -0,0 +1,13 @@
+#$ -cwd 
+#$ -V
+#$ -q short.qc
+#$ -pe shmem 2
+#$ -N notebook
+#$ -o notebook
+
+PORT=$(shuf -i10000-11999 -n1)
+echo "executing jupyter on http://$(hostname):$PORT"
+
+source /users/johnson/$USER/devel/venv/3.8.2-GCCcore-9.3.0/bin/activate
+
+jupyter-notebook --no-browser --port=$PORT --ip=`hostname -i`
